dcab4a91dbc6e2ef741a81a427bbbf8b73ef630b
Aragami1408 <vucaominh1408@gmail.com>
2025-03-28 10:58:51 +1100

    Done elementary arg parsing

--- search.py
+++ search.py
@@ -1,1 +1,17 @@
-print("Solution file")
+import sys
+
+
+def arg_parse():
+	argc = len(sys.argv)
+	if argc != 3:
+		print('Usage: python3 search.py <filename> <method>')
+	else:
+		filename = sys.argv[1]
+		method = sys.argv[2]
+
+	return (filename, method)
+
+def main():
+	filename, method = arg_parse()
+
+	print("{} {}".format(filename, method));
