1ac3f692d0e8e2ea457c9766ac5cbcf98f32bdf9
Aragami1408 <vucaominh1408@gmail.com>
2025-03-28 15:27:46 +1100

    Printing adheres to guidelines format

--- search.py
+++ search.py
@@ -1,6 +1,8 @@
 import sys
 from dataclasses import dataclass
 
+from uninformed import *
+
 @dataclass
 class Graph:
 	nodes: dict
@@ -75,8 +77,24 @@ def parse_graph(filename):
 def main():
 	try:
 		filename, method = parse_args()
+		print("{} {}".format(filename, method))
+
 		graph = parse_graph(filename)
-		print(graph)
+
+		result = []
+
+		for goal in graph.destinations:
+			match method:
+				case "bfs":
+					result = bfs(graph, graph.origin, goal)
+				case "dfs":
+					result = dfs(graph, graph.origin, goal)
+				case "_":
+					exit("Unknown method")
+
+			print("{} {}".format(goal, result))
+
+
 	except Exception as err:
 		print(f"Unexpected {err=}")
 
