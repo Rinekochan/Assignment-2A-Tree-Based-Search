570c04e9eab1ee2660916d62331cefc1c752b8c7
jjv7 <jaydenzikong@gmail.com>
2025-04-01 14:56:06 +1100

    change string interpolation method to fstring

--- search.py
+++ search.py
@@ -6,7 +6,7 @@ from utils import parse_args, parse_graph
 def main():
 	try:
 		filename, method = parse_args()
-		print("{} {}".format(filename, method))
+		print(f"{filename} {method}")
 
 		graph = parse_graph(filename)
 		#print(graph)
@@ -22,11 +22,11 @@ def main():
 				case "cus1":
 					result = dijkstra(graph, graph.origin, goal)
 				case "gbfs" | "as"| "cus2":
-					exit("The method {} is available but not implemented yet.".format(method))
+					exit(f"The method {method} is available but not implemented yet.")
 				case _:
 					exit("Unknown method, available methods: BFS, DFS, GBFS, AS, CUS1, CUS2")
 
-			print("{} {}".format(goal, len(result)))
+			print(f"{goal} {len(result)}")
 			print(result)
 
 
