a3a5b1e83bd69ee64fbd479e4fdd5be3f06c0532
jjv7 <jaydenzikong@gmail.com>
2025-04-08 16:16:15 +1000

    add dijkstra tests to batch file

--- runA2.bat
+++ runA2.bat
@@ -2,7 +2,7 @@
 
 echo Testing BFS:
 echo -------------------------------------
-for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15) do (
+for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17) do (
     python search.py Test/test_%%i.txt bfs
     echo.
 )
@@ -12,7 +12,7 @@ pause
 echo.
 echo Testing DFS:
 echo -------------------------------------
-for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15) do (
+for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17) do (
     python search.py Test/test_%%i.txt dfs
     echo.
 )
@@ -22,7 +22,7 @@ pause
 echo.
 echo Testing GBFS:
 echo -------------------------------------
-for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15) do (
+for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17) do (
     python search.py Test/test_%%i.txt gbfs
     echo.
 )
@@ -32,7 +32,7 @@ pause
 echo.
 echo Testing A*:
 echo -------------------------------------
-for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15) do (
+for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17) do (
     python search.py Test/test_%%i.txt as
     echo.
 )
@@ -42,7 +42,7 @@ pause
 echo.
 echo Testing CUS1 (Dijkstra):
 echo -------------------------------------
-for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15) do (
+for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17) do (
     python search.py Test/test_%%i.txt cus1
     echo.
 )
@@ -52,7 +52,7 @@ pause
 echo.
 echo Testing CUS2 (ACO):
 echo -------------------------------------
-for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15) do (
+for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17) do (
     python search.py Test/test_%%i.txt cus2
     echo.
 )
